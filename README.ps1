d5337d382aca72a3df36bfa2525e5475119cd90d
Mr. Proxy <80650301+Mr-Proxy-source@users.noreply.github.com>
2023-08-28 23:35:03 +0200

    Delete README.ps1

--- README.ps1
+++
@@ -1,8 +0,0 @@
-########################################################################################################################################################################
-#DISCLAIMER
-in
-#THIS REPOSITORY IS FOR EDUCATIONAL PURPOSES ONLY AND IS NOT INTENDED FOR REAL-WORLD USAGE. 
-#THE CREATORS OF THIS REPOSITORY ARE NOT RESPONSIBLE FOR ANY HARM OR DAMAGE THAT MAY Occur as a RESULT OF USING THE INFORMATION OR CODE PROVIDED IN THIS REPOSITORY.
-#BY ACCESSING AND USING THIS REPOSITORY, YOU ACKNOWLEDGE AND AGREE THAT YOU DO SO AT YOUR OWN RISK.
-#FAILURE TO COMPLY MAY RESULT IN SEVERE CONSEQUENCES!
-########################################################################################################################################################################
